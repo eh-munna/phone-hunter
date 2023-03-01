be0371d035fe101da9f681d8a90d753515b840b1
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-03-01 07:33:53 +0100

    slight design and api fetching

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 'use-strict';
+
+//getting IDs
+
+const getId = (id) => document.getElementById(id);
+
+// fetching data
+
+const fetchPhones = async () => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+  const response = await fetch(url);
+  const data = await response.json();
+  showPhones(data.data);
+};
+
+// showing phones
+const showPhones = (phones) => {
+  const phoneWrapper = getId('phone-wrapper');
+  phones.forEach((phone) => {
+    const { image, phone_name, brand } = phone;
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card p-2">
+      <img src="${image}" class="card-img-top" alt="..." />
+      <div class="card-body">
+          <h5 class="card-title">${phone_name}</h5>
+          <p class="card-text"> This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer</p>
+      </div>
+    </div>
+    
+    `;
+
+    phoneWrapper.appendChild(div);
+    console.log(phone);
+  });
+};
+
+fetchPhones();
